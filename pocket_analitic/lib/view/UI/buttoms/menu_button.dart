58b19ee492b10ce9c0705831b8c668c23de1c391
Nikita <pitsnikita@gmail.com>
2022-12-14 23:21:40 +0200

    created menu button file

---
+++ pocket_analitic/lib/view/UI/buttoms/menu_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateArgumentsPage extends StatefulWidget {
+  const CreateArgumentsPage({super.key});
+
+  @override
+  State<CreateArgumentsPage> createState() => _CreateArgumentsPageState();
+}
+
+class _CreateArgumentsPageState extends State<CreateArgumentsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
