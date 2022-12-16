fa090c19c34ff6d7bae951dee9e9f89b25ec7154
Nikita <pitsnikita@gmail.com>
2022-12-16 02:06:26 +0200

    changed main page

--- pocket_analitic/lib/home.dart
+++ pocket_analitic/lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pocket_analitic/theme/custom_colors.dart';
 import 'package:pocket_analitic/theme/custom_fonts.dart';
+import 'package:pocket_analitic/view/UI/buttoms/custom_icon_button.dart';
 import 'package:pocket_analitic/view/home_page_widgets/home_manu_buttons.dart';
 
 class Home extends StatefulWidget {
@@ -17,21 +18,42 @@ class _HomeState extends State<Home> {
       backgroundColor: CustomColors.bg,
       appBar: AppBar(
         backgroundColor: CustomColors.uiTheme,
-        title: Center(
-            child: Text(
-          'Pocket Analitic',
-          style: CustomTextStyle.title1ExtraBold24(c: CustomColors.bg),
-        )),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Pocket Analitic',
+              style: CustomTextStyle.title1ExtraBold24(c: CustomColors.bg),
+            ),
+            const Spacer(),
+            CustomIconButton(
+              callback: () {},
+              icon: 'assets/icons/icons8-info-50.svg',
+            ),
+          ],
+        ),
       ),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: const [
-            Center(
-              child: HomeMenuButtons(),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(top: 15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Welcome to "Pocket Analitic"',
+                  style:
+                      CustomTextStyle.title1Bold24(c: CustomColors.mainText)),
+              Container(
+                height: 300,
+                width: 300,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/logo.png')),
+                ),
+              ),
+              const HomeMenuButtons(),
+            ],
+          ),
         ),
       ),
     );
--- pocket_analitic/lib/theme/custom_colors.dart
+++ pocket_analitic/lib/theme/custom_colors.dart
@@ -2,10 +2,10 @@ import 'dart:ui';
 
 class CustomColors {
   static const Color bg = Color.fromARGB(255, 243, 248, 212);
+  static const Color black = Color.fromARGB(255, 30, 32, 32);
+  static const Color contures = Color.fromARGB(255, 178, 207, 219);
+  static const Color mainText = Color.fromARGB(255, 116, 29, 56);
   static const Color uiTheme = Color(0xFFBB2649);
   static const Color vivaMagenta = Color(0xFFBB2649);
-  static const Color contures = Color.fromARGB(255, 178, 207, 219);
-
-  static const Color black = Color.fromARGB(255, 30, 32, 32);
   static const Color white = Color.fromARGB(255, 248, 248, 238);
 }
